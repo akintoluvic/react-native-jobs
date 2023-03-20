2ff6a603c36a1fdb2857eb8ba96d06f6167878ab
Victor Akintunde <akintoluvic@gmail.com>
2023-03-20 11:57:17 +0100

    welcome style

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -14,9 +14,24 @@ import { icons, SIZES } from "../../../constants";
 import styles from "./welcome.style";
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Welcome Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect jobAdrian</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="what are you looking for?"
+          ></TextInput>
+        </View>
+      </View>
     </View>
   );
 };
