5151aa87bd18adaaf152df321d406f80b7a5b352
Victor Akintunde <akintoluvic@gmail.com>
2023-03-23 16:27:51 +0100

    fix error

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -16,7 +16,7 @@ const JobDetails = () => {
     const params = useSearchParams()
     const router = useRouter()
 
-    const { data, isLoading } = useFetch('job-details', {
+    const { data, isLoading, error, } = useFetch('job-details', {
         job_id: params.id
     })
 
@@ -51,7 +51,23 @@ const JobDetails = () => {
         <ScrollView
             showsVerticalScrollIndicator={false}
             refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-        ></ScrollView>
+        >
+            {isLoading ? (
+                <ActivityIndicator size='large' color={COLORS.primary} />
+            ) : error ? (
+                <Text>No Data</Text>
+            ) : (
+                <View style={{ padding:SIZES.medium, paddingBottom: 100}}>
+                    <Company
+                        companyLogo={data[0].employer_logo}
+                        jobTitle={data[0].job_title}
+                        companyName={data[0].employer_name}
+                        location={data[0].job_country}
+                    />
+                    <JobTabs />
+                </View>
+            )}
+        </ScrollView>
       </>
     </SafeAreaView>
   )
